e32f603eb04a4fc5af8c939ac638a7039966c765
arche416 <arche416@gmail.com>
2017-04-24 14:07:30 +0900

    Building funtions...!

--- fork_storm_master/storm-core/src/jvm/org/apache/storm/messaging/jxio/Client.java
+++ fork_storm_master/storm-core/src/jvm/org/apache/storm/messaging/jxio/Client.java
@@ -9,16 +9,58 @@ import org.apache.storm.grouping.Load;
 import org.apache.storm.messaging.ConnectionWithStatus;
 import org.apache.storm.messaging.IConnectionCallback;
 import org.apache.storm.messaging.TaskMessage;
+import org.apache.storm.messaging.org.accelio.jxio.ClientSession;
+import org.apache.storm.messaging.org.accelio.jxio.EventName;
+import org.apache.storm.messaging.org.accelio.jxio.EventQueueHandler;
+import org.apache.storm.messaging.org.accelio.jxio.EventReason;
+import org.apache.storm.messaging.org.accelio.jxio.Msg;
+import org.apache.storm.messaging.org.accelio.jxio.MsgPool;
 import org.apache.storm.metric.api.IStatefulObject;
 
 
 
 public class Client extends ConnectionWithStatus implements IStatefulObject {
-    
 
-    Client(Map stormConf, ScheduledThreadPoolExecutor scheduler, String host, int port, Context context) {
+	private EventQueueHandler eqh;
+	private ClientSession cs;
+	private MsgPool msgPool;
+	private EventName connectErrorType = null;
+	private boolean close = false;
+	private boolean established = false;
+	private Msg msg = null;
+	
+
+    public Client(Map stormConf, ScheduledThreadPoolExecutor scheduler, String host, int port, Context context) {
        
+    	cs = null;
+    }
+    
+    public class ClientCallbacks implements ClientSession.Callbacks {
 
+		@Override
+		public void onResponse(Msg msg) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onSessionEstablished() {
+			// TODO Auto-generated method stub
+			established = true;
+		}
+
+		@Override
+		public void onSessionEvent(EventName event, EventReason reason) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onMsgError(Msg msg, EventReason reason) {
+			// TODO Auto-generated method stub
+			
+		}
+    	
     }
 
 	@Override
@@ -67,5 +109,16 @@ public class Client extends ConnectionWithStatus implements IStatefulObject {
 	public Status status() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	private class Connect implements Runnable {
+		
+		private void reschedule() {
+			
+		}
+		
+		public void run() {
+			
+		}
 	}
 }
