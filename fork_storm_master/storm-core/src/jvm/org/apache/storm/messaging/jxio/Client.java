765e89148a736d93ca428d230ee50794d866a5ee
seokwoo <ysw424@gmail.com>
2017-05-11 01:19:25 +0900

    add some comments

--- fork_storm_master/storm-core/src/jvm/org/apache/storm/messaging/jxio/Client.java
+++ fork_storm_master/storm-core/src/jvm/org/apache/storm/messaging/jxio/Client.java
@@ -25,163 +25,163 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 
-
 public class Client extends ConnectionWithStatus implements IStatefulObject {
 
 
-	private EventQueueHandler eqh;
-	private ClientSession cs;
-	private MsgPool msgPool;
-	private AtomicBoolean close = new AtomicBoolean(false);
-	private AtomicBoolean established = new AtomicBoolean(false);
-	private Map stormConf;
-	private URI uri;
-	ScheduledThreadPoolExecutor scheduler;
-	
-	private volatile Map<Integer, Double> serverLoad = null;
+    private EventQueueHandler eqh;
+    private ClientSession cs;
+    private MsgPool msgPool;
+    private AtomicBoolean close = new AtomicBoolean(false);
+    private AtomicBoolean established = new AtomicBoolean(false);
+    private Map stormConf;
+    private URI uri;
+    ScheduledThreadPoolExecutor scheduler;
+
+    private volatile Map<Integer, Double> serverLoad = null;
 
     public Client(Map stormConf, ScheduledThreadPoolExecutor scheduler, String host, int port, Context context) {
 
-    	this.stormConf = stormConf;
-    	
-    	try {
-			uri = new URI(String.format("rdma://%s:%s", host, port));
-		} catch (URISyntaxException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	
-    	this.scheduler = scheduler;
-    	eqh = new EventQueueHandler(null);
-		msgPool = new MsgPool(Utils.getInt(stormConf.get(Config.STORM_MEESAGING_JXIO_MSGPOOL_BUFFER_SIZE)), 
-				Utils.getInt(stormConf.get(Config.STORM_MESSAGING_JXIO_CLIENT_INPUT_BUFFER_COUNT)), 
-				Utils.getInt(stormConf.get(Config.STORM_MESSAGING_JXIO_CLIENT_OUTPUT_BUFFER_COUNT)));
-    	connect(0);
+        this.stormConf = stormConf;
+
+        try {
+            uri = new URI(String.format("rdma://%s:%s", host, port));
+        } catch (URISyntaxException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+//		LOG.info("creating Netty Client, connecting to {}:{}, bufferSize: {}", host, port, bufferSize);
+        this.scheduler = scheduler;
+        eqh = new EventQueueHandler(null);
+        msgPool = new MsgPool(Utils.getInt(stormConf.get(Config.STORM_MEESAGING_JXIO_MSGPOOL_BUFFER_SIZE)),
+                Utils.getInt(stormConf.get(Config.STORM_MESSAGING_JXIO_CLIENT_INPUT_BUFFER_COUNT)),
+                Utils.getInt(stormConf.get(Config.STORM_MESSAGING_JXIO_CLIENT_OUTPUT_BUFFER_COUNT)));
+        connect(0);
     }
-    private ScheduledFuture<?> connect(long delayMs){
-    	cs = new ClientSession(eqh, uri, new ClientCallbacks() );
-    	return scheduler.schedule(eqh, delayMs, TimeUnit.MILLISECONDS);
-    	
+
+    private ScheduledFuture<?> connect(long delayMs) {
+        cs = new ClientSession(eqh, uri, new ClientCallbacks());
+        return scheduler.schedule(eqh, delayMs, TimeUnit.MILLISECONDS);
+
     }
-    
+
     class ClientCallbacks implements ClientSession.Callbacks {
 
-		@Override
-		public void onResponse(Msg msg) {
-			// TODO Auto-generated method stub
-			msg.returnToParentPool();
-		}
+        @Override
+        public void onResponse(Msg msg) {
+            // TODO Auto-generated method stub
+            //StormClientHandler 참조
+            msg.returnToParentPool();
+        }
 
-		@Override
-		public void onSessionEstablished() {
-			// TODO Auto-generated method stub
-			established.set(true);
-		}
+        @Override
+        public void onSessionEstablished() {
+            // TODO Auto-generated method stub
+            established.set(true);
+        }
 
-		@Override
-		public void onSessionEvent(EventName event, EventReason reason) {
-			// TODO Auto-generated method stub
-			if(event == EventName.SESSION_CLOSED || event == EventName.SESSION_ERROR 
-					|| event == EventName.SESSION_REJECT){
-				if(!close.get())
-				{
-					eqh.stop();
-					connect(0);
-				}
-				
-			}
-		}
+        @Override
+        public void onSessionEvent(EventName event, EventReason reason) {
+            // TODO Auto-generated method stub
+            if (event == EventName.SESSION_CLOSED || event == EventName.SESSION_ERROR
+                    || event == EventName.SESSION_REJECT) {
+                if (!close.get()) {
+                    eqh.stop();
+                    connect(0);
+                }
 
-		@Override
-		public void onMsgError(Msg msg, EventReason reason) {
-			// TODO Auto-generated method stub
-			msg.returnToParentPool();
-		}
-    	
+            }
+        }
+
+        @Override
+        public void onMsgError(Msg msg, EventReason reason) {
+            // TODO Auto-generated method stub
+            msg.returnToParentPool();
+        }
+
     }
 
-	@Override
-	public void registerRecv(IConnectionCallback cb) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Client connection should not receive any messages");
-	}
+    @Override
+    public void registerRecv(IConnectionCallback cb) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Client connection should not receive any messages");
+    }
 
-	@Override
-	public void sendLoadMetrics(Map<Integer, Double> taskToLoad) {
-		// TODO Auto-generated method stub
-		throw new RuntimeException("Client connection should not send load metrics");
-		
-	}
+    @Override
+    public void sendLoadMetrics(Map<Integer, Double> taskToLoad) {
+        // TODO Auto-generated method stub
+        throw new RuntimeException("Client connection should not send load metrics");
 
-	@Override
-	public void send(int taskId, byte[] payload) {
-		// TODO Auto-generated method stub
-		TaskMessage msg = new TaskMessage(taskId, payload);
+    }
+
+    @Override
+    public void send(int taskId, byte[] payload) {
+        // TODO Auto-generated method stub
+        TaskMessage msg = new TaskMessage(taskId, payload);
         List<TaskMessage> wrapper = new ArrayList<TaskMessage>(1);
         wrapper.add(msg);
         send(wrapper.iterator());
-	}
+    }
 
-	@Override
-	public void send(Iterator<TaskMessage> msgs) {
-		// TODO Auto-generated method stub
-		if(close.get()) {
-			return;
-		}
-		if(!hasMessages(msgs)) {
-			return;
-		}
-		if(cs == null) {
-			return;
-		}
-		while(msgs.hasNext()) {
-			Msg msg = msgPool.getMsg();
-			msg.getOut().put(msgs.next().serialize().array());
-			
-			try {
-				cs.sendRequest(msg);
-			} catch (JxioGeneralException | JxioSessionClosedException | JxioQueueOverflowException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				msg.returnToParentPool();
-			}
-			
-		}
-		
-	}
-	
-	private boolean hasMessages(Iterator<TaskMessage> msgs) {
+    @Override
+    public void send(Iterator<TaskMessage> msgs) {
+        // TODO Auto-generated method stub
+        if (close.get()) {
+            return;
+        }
+        if (!hasMessages(msgs)) {
+            return;
+        }
+        if (cs == null) {
+            return;
+        }
+        while (msgs.hasNext()) {
+            Msg msg = msgPool.getMsg();
+            msg.getOut().put(msgs.next().serialize().array());
+
+            try {
+                cs.sendRequest(msg);
+            } catch (JxioGeneralException | JxioSessionClosedException | JxioQueueOverflowException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                msg.returnToParentPool();
+            }
+
+        }
+
+    }
+
+    private boolean hasMessages(Iterator<TaskMessage> msgs) {
         return msgs != null && msgs.hasNext();
     }
 
-	@Override
-	public Map<Integer, Load> getLoad(Collection<Integer> tasks) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Map<Integer, Load> getLoad(Collection<Integer> tasks) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public void close() {
-		// TODO Auto-generated method stub
-		close.set(true);
-		cs.close();
-		eqh.close();
-		msgPool.deleteMsgPool();
-	}
+    @Override
+    public void close() {
+        // TODO Auto-generated method stub
+        close.set(true);
+        cs.close();
+        eqh.close();
+        msgPool.deleteMsgPool();
+    }
 
-	@Override
-	public Object getState() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Object getState() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public Status status() {
-		// TODO Auto-generated method stub
-		if(close.get()) return Status.Closed;
-		else if(established.get()) return Status.Connecting;
-		else return Status.Ready;
-		
-	}
-	
+    @Override
+    public Status status() {
+        // TODO Auto-generated method stub
+        if (close.get()) return Status.Closed;
+        else if (established.get()) return Status.Connecting;
+        else return Status.Ready;
+
+    }
+
 }
