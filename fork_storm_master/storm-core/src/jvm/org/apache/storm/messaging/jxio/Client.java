9c2904be61e9bac8b3dd282107af4791144a3177
arche416 <arche416@gmail.com>
2017-05-10 22:55:05 +0900

    edit cliet

--- fork_storm_master/storm-core/src/jvm/org/apache/storm/messaging/jxio/Client.java
+++ fork_storm_master/storm-core/src/jvm/org/apache/storm/messaging/jxio/Client.java
@@ -22,7 +22,6 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
