1118b6bc819e7b2fc1807080012622b34f06316b
0KUK <ygkim@kangwon.ac.kr>
2017-11-14 22:33:32 +0900

    [Kafka] Consumter 수정

--- i2am-performance-test/benchmark-storm-and-spark/benchmark-kafka/src/main/java/i2am/benchmark/kafka/ConsumerRunner.java
+++ i2am-performance-test/benchmark-storm-and-spark/benchmark-kafka/src/main/java/i2am/benchmark/kafka/ConsumerRunner.java
@@ -91,6 +91,7 @@ public class ConsumerRunner implements Runnable {
 									new String(messageAndMetadata.message()));
 							System.out.println(messages.toJSONString());
 							
+							// Get
 							String wordcount = ((JSONObject) messages.get("tweet")).toJSONString();							
 							int production = ((Number) messages.get("production")).intValue();
 							long createdTime = ((Number) messages.get("createdTime")).longValue();
