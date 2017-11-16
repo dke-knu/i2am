a6a9d195a9069bfa59dbea8c9c684fcb959d799a
whcho <tmzkdls7989@naver.com>
2017-11-16 16:11:04 +0900

    메인 함수

---
+++ i2am-app/algorithm-selection-engine/AlgorithmSelectionEngine.py
@@ -0,0 +1,31 @@
+import socket
+import threading
+
+
+HOST = '114.70.235.43'
+PORT = 7979
+BUFFERSIZE = 1024
+ADDRESS = (HOST, PORT)
+
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("Binding")
+serverSocket.bind(ADDRESS)
+print("Listening")
+serverSocket.listen(1)
+
+def samplingAlgorithmSelect(clientSocket, address):
+    print("Start Sampling Algorithm Selection Engine for", address)
+    data = clientSocket.recv()
+    if data == '1':
+
+        # Deep Learning Engine
+
+while True:
+    clientSocket, address = serverSocket.accept()
+    print("Connected by", address)
+    thread = threading.Thread(target=samplingAlgorithmSelect, args=(clientSocket, address))
+    thread.start()
+
+
+
+
