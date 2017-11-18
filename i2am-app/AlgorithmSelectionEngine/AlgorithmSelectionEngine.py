3c1cff65d345b265d19604d62804f796cc567096
whcho <tmzkdls7989@naver.com>
2017-11-18 19:40:08 +0900

    DB 연결

--- i2am-app/AlgorithmSelectionEngine/AlgorithmSelectionEngine.py
+++ i2am-app/AlgorithmSelectionEngine/AlgorithmSelectionEngine.py
@@ -1,30 +1,35 @@
 import socket
 import threading
+import pymysql
 
 HOST = '114.70.235.43'
 PORT = 7979
 BUFFERSIZE = 1024
 ADDRESS = (HOST, PORT)
 
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print("Binding")
-serverSocket.bind(ADDRESS)
-print("Listening")
-serverSocket.listen(1)
+# serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# print("Binding")
+# serverSocket.bind(ADDRESS)
+# print("Listening")
+# serverSocket.listen(1)
+
+# Connect MariaDB
+db = pymysql.connect(host = '114.70.235.43', port = 3306, user = 'plan-manager', passwd = 'dke214', db = 'i2am', charset = 'utf8', autocommit = True)
+cursor = db.cursor()
+getFilePathSQL = "SELECT FILE_PATH FROM tbl_src_test_data WHERE NAME = %s AND F_OWNER = (SELECT IDX FROM tbl_user WHERE ID = %s)"
+cursor.execute(getFilePathSQL, ('abc', 'abc@naver.com'))
+filePath = cursor.fetchone()
+print(filePath)
+cursor.close()
 
 def samplingAlgorithmSelect(clientSocket, address):
     print("Start Sampling Algorithm Selection Engine for", address)
     data = clientSocket.recv()
 
-
-
-
-while True:
-    clientSocket, address = serverSocket.accept()
-    print("Connected by", address)
-    thread = threading.Thread(target=samplingAlgorithmSelect, args=(clientSocket, address))
-    thread.start()
-
-
+# while True:
+#     clientSocket, address = serverSocket.accept()
+#     print("Connected by", address)
+#     thread = threading.Thread(target=samplingAlgorithmSelect, args=(clientSocket, address))
+#     thread.start()
 
 
