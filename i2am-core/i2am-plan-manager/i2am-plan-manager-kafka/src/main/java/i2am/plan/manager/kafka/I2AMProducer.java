1a4278c48ae9462ef5ddb845160078f0b3b1c441
Siwoon <Siwoon@DESKTOP-BBN7S21>
2018-02-06 15:09:13 +0900

    added comments for load-shedding engine.

--- i2am-core/i2am-plan-manager/i2am-plan-manager-kafka/src/main/java/i2am/plan/manager/kafka/I2AMProducer.java
+++ i2am-core/i2am-plan-manager/i2am-plan-manager-kafka/src/main/java/i2am/plan/manager/kafka/I2AMProducer.java
@@ -12,6 +12,8 @@ public class I2AMProducer {
 	
 	private Producer<String, String> producer;
 
+	// 쓰레드에서 set, send 메서드에서 get할 수 있는 스위치 공유 변수 필요
+	
 	public I2AMProducer(String id, String srcName) {
 		this.brokers = "114.70.235.43:19092,114.70.235.43:19093,114.70.235.43:19094,"
 				+ "114.70.235.43:19095,114.70.235.43:19096,114.70.235.43:19097,"
@@ -24,19 +26,23 @@ public class I2AMProducer {
 
 		ProducerConfig producerConfig = new ProducerConfig(props);
 		producer = new Producer<String, String>(producerConfig);
+		
+		// 쓰레드 시작
 	}
 
 	public void send(String message) {
 		if (this.topic == null) {
 			throw new NullPointerException("Source is not available.");
 		}
+		// 스위치가 true일 때 send
 		producer.send(new KeyedMessage<String, String>(this.topic, message));
 	}
 
-	public String getInputTopic(String id, String srcName) {
-		return DbAdapter.getInstance().getInputTopic(id, srcName);
+	private String getInputTopic(String id, String srcName) {
+		return getDbInstance().getInputTopic(id, srcName);
+	}
+	
+	private DbAdapter getDbInstance() {
+		return DbAdapter.getInstance();
 	}
 }
-
-
-
