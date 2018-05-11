962d788a177b7a4aff8075f48cdeabe78380598d
Siwoon <Siwoon@DESKTOP-BBN7S21>
2018-05-11 10:48:50 +0900

    added MultivariateQueryFilteringBolt

---
+++ i2am-core/i2am-algorithm-filtering/src/main/java/i2am/Filtering/MultivariateQueryFilteringBolt.java
@@ -0,0 +1,60 @@
+package i2am.Filtering;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.storm.redis.common.config.JedisClusterConfig;
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseRichBolt;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Tuple;
+import org.apache.storm.tuple.Values;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import i2am.query.parser.DbAdapter;
+import i2am.query.parser.Node;
+
+public class MultivariateQueryFilteringBolt extends BaseRichBolt{
+	private final static Logger logger = LoggerFactory.getLogger(MultivariateQueryFilteringBolt.class);
+
+	private String topologyName;
+
+    private OutputCollector collector;
+    private Node query;
+    private String[] schema;
+
+    public MultivariateQueryFilteringBolt(String topologyName){
+        this.topologyName = topologyName;
+    }
+
+    @Override
+    public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
+        this.collector = collector;
+        this.query = DbAdapter.getInstance().getQuery(topologyName);
+        this.schema = DbAdapter.getInstance().getSchema(topologyName);
+    }
+
+    @Override
+    public void execute(Tuple input) {
+        String data = input.getString(0);
+        if ( !query.evaluate(arraysToMap(schema, data.split(","))) )
+        	return;
+        collector.emit(new Values(data));
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+        declarer.declare(new Fields("data"));
+    }
+    
+    private Map<String, String> arraysToMap (String[] key, String[] value) {
+    	Map<String, String> map = new HashMap<String, String>();
+    	for (int i=0; i<value.length; i++) {
+    		map.put(key[i], value[i]);
+    	}
+    	return map;
+    }
+}
