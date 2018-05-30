1e2b0132cffc35c893ec47e9c6ed69b2d61497da
Siwoon <Siwoon@DESKTOP-BBN7S21>
2018-05-30 17:40:38 +0900

    init load-shedding-engine project.

---
+++ i2am-core/i2am-load-shedding-engine/src/main/java/org/i2am/load/shedding/engine/App.java
@@ -0,0 +1,13 @@
+package org.i2am.load.shedding.engine;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
