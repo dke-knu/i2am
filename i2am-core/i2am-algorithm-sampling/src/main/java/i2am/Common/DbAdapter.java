7057a0246928a717363d0544eaa7b7f04d04a803
whcho <tmzkdls7989@naver.com>
2018-07-10 18:49:07 +0900

    DB ADAPTER 수정

--- i2am-core/i2am-algorithm-sampling/src/main/java/i2am/Common/DbAdapter.java
+++ i2am-core/i2am-algorithm-sampling/src/main/java/i2am/Common/DbAdapter.java
@@ -14,8 +14,7 @@ public class DbAdapter {
     private final DataSource ds;
     private volatile static DbAdapter instance;
 
-    private static final String GETTARGETQUERY = "SELECT F_TARGET FROM tbl_intelligent_engine WHERE F_SRC = (SELECT F_SRC FROM tbl_plan WHERE IDX = (SELECT F_PLAN FROM tbl_topology WHERE TOPOLOGY_NAME = ?))";
-    private static final String GETTARGETINDEXQUERY = "SELECT COLUMN_INDEX FROM tbl_src_csv_schema WHERE IDX = ?";
+    private static final String GETTARGETINDEXQUERY = "SELECT COLUMN_INDEX FROM tbl_src_csv_schema WHERE IDX = (SELECT F_TARGET FROM tbl_intelligent_engine WHERE F_SRC = (SELECT F_SRC FROM tbl_plan WHERE IDX = (SELECT F_PLAN FROM tbl_topology WHERE TOPOLOGY_NAME = ?)))";
     private static final String GETHASHFUNCTIONQUERY = "SELECT HASH_FUNCTION FROM tbl_params_hash_sampling WHERE IDX = (SELECT IDX FROM tbl_topology WHERE TOPOLOGY_NAME = ?)";
 
     private DbAdapter(){
@@ -34,22 +33,10 @@ public class DbAdapter {
         return instance;
     }
 
-    public String getTarget(String topologyName) throws SQLException {
-        connection = ds.getConnection();
-        preparedStatement = connection.prepareStatement(GETTARGETQUERY);
-        preparedStatement.setString(1, topologyName);
-        resultSet = preparedStatement.executeQuery();
-        String targetName = resultSet.getString("F_TARGET");
-        preparedStatement.close();
-        resultSet.close();
-        connection.close();
-        return targetName;
-    }
-
-    public int getTargetIndex(String targetName) throws SQLException {
+    public int getTargetIndex(String topologyName) throws SQLException {
         connection = ds.getConnection();
         preparedStatement = connection.prepareStatement(GETTARGETINDEXQUERY);
-        preparedStatement.setString(1, targetName);
+        preparedStatement.setString(1, topologyName);
         resultSet = preparedStatement.executeQuery();
         int targetIndex = resultSet.getInt("COLUMN_INDEX");
         preparedStatement.close();
