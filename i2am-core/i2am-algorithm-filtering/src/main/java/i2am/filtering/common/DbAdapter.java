94272a9bd05ad67cb5b527602d4ff8044b2fb860
whcho <tmzkdls7989@naver.com>
2018-09-12 18:51:36 +0900

    타겟 쿼리 수정

--- i2am-core/i2am-algorithm-filtering/src/main/java/i2am/filtering/common/DbAdapter.java
+++ i2am-core/i2am-algorithm-filtering/src/main/java/i2am/filtering/common/DbAdapter.java
@@ -14,7 +14,7 @@ public class DbAdapter {
     private final DataSource ds;
     private volatile static DbAdapter instance;
 
-    private static final String GETTARGETINDEXQUERY = "SELECT COLUMN_INDEX FROM tbl_src_csv_schema WHERE IDX = (SELECT F_TARGET FROM ? WHERE F_TOPOLOGY = (SELECT IDX FROM tbl_topology WHERE TOPOLOGY_NAME = ?))";
+//    private static final String GETTARGETINDEXQUERY = "SELECT COLUMN_INDEX FROM tbl_src_csv_schema WHERE IDX = (SELECT F_TARGET FROM  WHERE F_TOPOLOGY = (SELECT IDX FROM tbl_topology WHERE TOPOLOGY_NAME = ?))";
     private static final String GETBLOOMHASHFUNCTIONQUERY = "SELECT HASH_FUNCTION1, HASH_FUNCTION2, HASH_FUNCTION3 FROM tbl_params_bloom_filtering WHERE F_TOPOLOGY = (SELECT IDX FROM tbl_topology WHERE TOPOLOGY_NAME = ?)";
 
     private DbAdapter(){
@@ -34,26 +34,33 @@ public class DbAdapter {
     }
 
     public int getTargetIndex(String topologyName, String algorithmName) throws SQLException {
-        String tableName = "";
+        String query = null;
         int targetIndex = 0;
         connection = ds.getConnection();
-        preparedStatement = connection.prepareStatement(GETTARGETINDEXQUERY);
         switch (algorithmName) {
             case "BLOOM_FILTERING":
-                tableName = "tbl_params_bloom_filtering";
+                query = "SELECT COLUMN_INDEX FROM tbl_src_csv_schema WHERE IDX = " +
+                        "(SELECT F_TARGET FROM tbl_params_bloom_filtering WHERE F_TOPOLOGY = " +
+                        "(SELECT IDX FROM tbl_topology WHERE TOPOLOGY_NAME = ?))";
                 break;
             case "KALMAN_FILTERING":
-                tableName = "tbl_params_kalman_filtering";
+                query = "SELECT COLUMN_INDEX FROM tbl_src_csv_schema WHERE IDX = " +
+                        "(SELECT F_TARGET FROM tbl_params_kalman_filtering WHERE F_TOPOLOGY = " +
+                        "(SELECT IDX FROM tbl_topology WHERE TOPOLOGY_NAME = ?))";
                 break;
             case "NR_KALMAN_FILTERING":
-                tableName = "tbl_params_noise_recommend_kalman_filtering";
+                query = "SELECT COLUMN_INDEX FROM tbl_src_csv_schema WHERE IDX = " +
+                        "(SELECT F_TARGET FROM tbl_params_noise_recommend_kalman_filtering WHERE F_TOPOLOGY = " +
+                        "(SELECT IDX FROM tbl_topology WHERE TOPOLOGY_NAME = ?))";
                 break;
             case "I_KALMAN_FILTERING":
-                tableName = "tbl_intelligent_kalman_filtering";
+                query = "SELECT COLUMN_INDEX FROM tbl_src_csv_schema WHERE IDX = " +
+                        "(SELECT F_TARGET FROM tbl_params_intelligent_kalman_filtering WHERE F_TOPOLOGY = " +
+                        "(SELECT IDX FROM tbl_topology WHERE TOPOLOGY_NAME = ?))";
                 break;
         }
-        preparedStatement.setString(1, tableName);
-        preparedStatement.setString(2, topologyName);
+        preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, topologyName);
         resultSet = preparedStatement.executeQuery();
         if(resultSet.next()) {
             targetIndex = resultSet.getInt("COLUMN_INDEX");
