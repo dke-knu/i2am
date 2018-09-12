9fa4552ea05dedb05abddf9d1236fdd498c02180
whcho <tmzkdls7989@naver.com>
2018-09-12 19:00:48 +0900

    타겟 쿼리 수정

--- i2am-core/i2am-algorithm-sampling/src/main/java/i2am/sampling/common/DbAdapter.java
+++ i2am-core/i2am-algorithm-sampling/src/main/java/i2am/sampling/common/DbAdapter.java
@@ -14,7 +14,7 @@ public class DbAdapter {
     private final DataSource ds;
     private volatile static DbAdapter instance;
 
-    private static final String GETTARGETINDEXQUERY = "SELECT COLUMN_INDEX FROM tbl_src_csv_schema WHERE IDX = (SELECT F_TARGET FROM ? WHERE F_TOPOLOGY = (SELECT IDX FROM tbl_topology WHERE TOPOLOGY_NAME = ?))";
+//    private static final String GETTARGETINDEXQUERY = "SELECT COLUMN_INDEX FROM tbl_src_csv_schema WHERE IDX = (SELECT F_TARGET FROM ? WHERE F_TOPOLOGY = (SELECT IDX FROM tbl_topology WHERE TOPOLOGY_NAME = ?))";
     private static final String GETHASHFUNCTIONQUERY = "SELECT HASH_FUNCTION FROM tbl_params_hash_sampling WHERE F_TOPOLOGY = (SELECT IDX FROM tbl_topology WHERE TOPOLOGY_NAME = ?)";
 
     private DbAdapter(){
@@ -34,19 +34,22 @@ public class DbAdapter {
     }
 
     public int getTargetIndex(String topologyName, String algorithmName) throws SQLException {
-        String tableName = "";
+        String query = null;
         connection = ds.getConnection();
-        preparedStatement = connection.prepareStatement(GETTARGETINDEXQUERY);
         switch (algorithmName) {
             case "HASH_SAMPLING":
-                tableName = "tbl_params_hash_sampling";
+                query = "SELECT COLUMN_INDEX FROM tbl_src_csv_schema WHERE IDX = " +
+                        "(SELECT F_TARGET FROM tbl_params_hash_sampling WHERE F_TOPOLOGY = " +
+                        "(SELECT IDX FROM tbl_topology WHERE TOPOLOGY_NAME = ?))";
                 break;
             case "PRIORITY_SAMPLING":
-                tableName = "tbl_params_priority_sampling";
+                query = "SELECT COLUMN_INDEX FROM tbl_src_csv_schema WHERE IDX = " +
+                        "(SELECT F_TARGET FROM tbl_params_priority_sampling WHERE F_TOPOLOGY = " +
+                        "(SELECT IDX FROM tbl_topology WHERE TOPOLOGY_NAME = ?))";
                 break;
         }
-        preparedStatement.setString(1, tableName);
-        preparedStatement.setString(2, topologyName);
+        preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, topologyName);
         resultSet = preparedStatement.executeQuery();
         int targetIndex = 0;
         if(resultSet.next())
